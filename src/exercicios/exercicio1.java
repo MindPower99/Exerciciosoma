2d8b04bed05afdb83fe04bc3a1d00072605d4c68
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-08-15 20:38:29 -0300

    Exemplo com JOptionPane

--- src/exercicios/exercicio1.java
+++ src/exercicios/exercicio1.java
@@ -1,11 +1,11 @@
-package exercicios;
-import java.util.Scanner;
+package exercicios;//indicando que está no pacote exercicios
+import java.util.Scanner; //importando o Scanner
 
-public class exercicio1 {
+public class exercicio1 { //criando a classe exercicio1
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int n1 = 0 , n2 = 0, result = 0;
+	public static void main(String[] args) { // método principal - classe publica, estatica e sem retorno
+		Scanner input = new Scanner(System.in); // instanciando o scanner (pra 'ler' os valores)
+		int n1 = 0 , n2 = 0, result = 0; 
 		System.out.println("Digite o primeiro número: ");
 		n1 = input.nextInt();
 		System.out.println("Digite o próximo número: ");
